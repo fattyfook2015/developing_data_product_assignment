436b208fa2bd21aec69c1383dea434ff36310dab
leesinfook2015 <leesinfook@gmail.com>
2015-09-26 16:46:10 +0800

    uploading of all the slides and server and ui

---
+++ first_version_slide/test1.R
@@ -0,0 +1,7 @@
+library("devtools")
+library("slidify")
+library("knitr")
+
+setwd("C:/Users/User/Desktop/school/IDA_MOOC/coursera2015/9.Developing Data Products/slidify_proj")
+
+author("first_version_slide")
